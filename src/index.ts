71c7b0611a71db92d305362e8a07d60e0bdadc4f
JonaVS <jon4963@gmail.com>
2023-03-01 15:09:09 -0600

    feat: add MongoDB base config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { config } from "dotenv";
+config();
 import express from "express";
 import { getHelloMessage } from "./controllers/helloController.js";
+import mongoose from "mongoose";
 
 const PORT = 5000;
 const app = express();
@@ -8,4 +11,9 @@ app.use(express.json());
 
 app.get("/", getHelloMessage);
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+} catch (error) {
+  console.error(error);
+}
