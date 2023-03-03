c65539ed89b580fc7dfcc3187fea65731cfce9ff
JonaVS <jon4963@gmail.com>
2023-03-02 23:26:17 -0600

    fix: flag url as invalid if no http or https protocol

--- src/utils/urlFormatValidator.ts
+++ src/utils/urlFormatValidator.ts
@@ -2,8 +2,12 @@ export const isValidUrlFormat = (url: string): boolean => {
   try {
     const parsedUrl = new URL(url);
 
-    const domainRegex = new RegExp(`^(${process.env.API_DOMAIN}|${process.env.APP_DOMAIN})$`) 
-    if (parsedUrl.hostname.match(domainRegex)) {
+    const domainRegex = new RegExp(`^(${process.env.API_DOMAIN}|${process.env.APP_DOMAIN})$`)
+    const protocolRegex = new RegExp(/^(https?:)$/i) 
+    if (
+      parsedUrl.hostname.match(domainRegex) ||
+      !parsedUrl.protocol.match(protocolRegex)
+    ) {
       return false;
     }
 
