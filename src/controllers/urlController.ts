254aeb5b847eb36cc2861d2c40151bb746666a9b
JonaVS <jon4963@gmail.com>
2023-03-04 13:50:22 -0600

    feat: add findOriginalUrl method at urlController

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -1,7 +1,11 @@
-import { CreateUrlDTO, UrlDTO } from "../dto/Url/UrlDtos.js";
+import { CreateUrlDTO, OriginalUrlDTO, UrlDTO } from "../dto/Url/UrlDtos.js";
 import { ActionResult } from "../types/ActionResult.js";
 import * as urlService from "../db/services/urlService.js"
 
 export const createUrl = async (payload: CreateUrlDTO):Promise<ActionResult<UrlDTO | null>> => {
   return await urlService.createUrl(payload);
+}
+
+export const findOrinalUrl = async (shortUrlId: string):Promise<ActionResult<OriginalUrlDTO | null>> => {
+  return await urlService.findOriginalUrl(shortUrlId);
 }
