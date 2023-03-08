d043405a70477eb3255242eae01a18c08bcceafe
JonaVS <jon4963@gmail.com>
2023-03-08 09:56:40 -0600

    feat: use certain db depending on environment

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,11 @@ app.use(express.json());
 app.use("/", router)
 
 try {
-  await mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_URL
+      : process.env.DEV_MONGO_URL
+  );
   app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
 } catch (error) {
   console.error(error);
