5c82474ed9af8eecb9380c351e38e18ddd818218
JonaVS <jon4963@gmail.com>
2023-03-08 09:53:14 -0600

    feat: add type for DEV_MONGO_URL env var

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,7 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      DEV_MONGO_URL: string
       MONGO_URL: string,
       API_DOMAIN: string,
       APP_DOMAIN: string,
